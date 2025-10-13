d0f763eb68172011819934cf746ba92b312e89c2
Bendi156 <toth.peter.bendeguz.2022i@bankitatabanya.hu>
2025-10-13 10:24:55 +0200

    first commit

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Game {
+    public static void main(String[] args) {
+        //git remote add origin URL
+        //git remote add origin https://github.com/Bendi156/worldofyesking.git
+    }
+}
