dfa0c264cf20fdc3cb96890b8b8944dbb200046f
Bendi156 <toth.peter.bendeguz.2022i@bankitatabanya.hu>
2025-10-15 13:06:09 +0200

    Kilépés

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Game {
@@ -5,6 +8,33 @@ public class Game {
         //git remote add origin URL
         //git remote add origin https://github.com/Bendi156/worldofyesking.git
 
-
+        Game game = new Game();
+        game.play();
     }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+        display("Üdv a várkalandban!");
+
+        // ez maga a játék ciklus
+        while(true){
+
+            String input = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+            String command = input;
+
+
+            switch (command){
+                case "kilep":
+                    display("Köszi a játékot!");
+                    scanner.close();
+                    return;
+            }
+        }
+    }
+
+    private void display(String message) {
+        System.out.println(message);
+    }
+
+
 }
