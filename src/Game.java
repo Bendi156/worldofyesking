8fe6eb9d6d4c78fff64521d1c55cdb0978fc1813
Bendi156 <toth.peter.bendeguz.2022i@bankitatabanya.hu>
2025-10-22 13:08:15 +0200

    init some scenes

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,14 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Game {
+
     Player player;
-    public Game(){
-        Scene entrance = new Scene("A vár bejáratánál állsz! Északra van egy nagy faajtó található");
-        Scene hall = new Scene("A nagyteremben vagy. Keltre és Nyugatra is van egy-egy ajtó");
-        Scene armory = new Scene("A fegyvertárban vagy. Látsz egy fényes kardot az állványon");
+
+    public Game() {
+        Scene entrance = new Scene("A vár bejáratánál állsz. És északra egy nagy faajtó található.");
+        Scene hall = new Scene("A nagyteremben vagy. Keletre és nyugatra is van egy-egy ajtó.");
+        Scene armory = new Scene("A fegyvertárban vagy. Látsz egy fényes kardot az állványon.");
 
         entrance.setExit(Direction.ESZAK, hall);
         hall.setExit(Direction.DEL, entrance);
@@ -16,44 +16,36 @@ public class Game {
         armory.setExit(Direction.KELET, hall);
 
         player = new Player(entrance);
-
-    }
-
-
-    public static void main(String[] args) {
-        //git remote add origin URL
-        //git remote add origin https://github.com/Bendi156/worldofyesking.git
-
-        Game game = new Game();
-        game.play();
     }
 
     private void play() {
         Scanner scanner = new Scanner(System.in);
-        display("Üdv a várkalandban!");
-        display(player.getCurrentScene().getDescription());
+        display("Üdv a várkalandban");
+
 
         // ez maga a játék ciklus
-        while(true){
-            display("............................");
+        while (true) {
+            display("----------------------------");
             display(player.getCurrentScene().getDescription());
             display(">");
 
+            //Scanner nextLine() az mindig egy teljes sort olvas be
             String input = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
-            String[] words = input.split("");
+            String[] words = input.split(" ");
 
             String command = words[0];
 
-            // Ha van második szó itt kinyerjük termary operator-ral -> termary operator az ugyanaz mint egy if-ben, csak tömör
+            //Ha van második szó , majd itt kinyerjük egy terrary operator-ral -> terrary ugyanaz, mint egy if-ben, csak tömör
             String subject = words.length > 1 ? words[1] : "";
 
-            switch (command){
+            switch (command) {
+
                 case "menj":
+                    //TODO moveplayer
                     Direction direction = Direction.fromString(subject);
                     moveplayer(direction);
-                    break; // ha nincs akkor tovább megy a következő ágra.
-
-                case "kilep":
+                    break;
+                case "kilép":
                     display("Köszi a játékot!");
                     scanner.close();
                     return;
@@ -67,7 +59,7 @@ public class Game {
     private void moveplayer(Direction direction) {
         Scene nextScene = player.getCurrentScene().getExit(direction);
         if(nextScene == null){
-            display("Nem mehetsz arra");
+            display("Nem mehetsz arra!");
         }
         player.setScene(nextScene);
     }
@@ -76,4 +68,9 @@ public class Game {
         System.out.println(message);
     }
 
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.play();
+    }
 }
